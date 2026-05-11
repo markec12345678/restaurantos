66558c90966cbeb13843a0f5dcf311fb0c424214
Z User <z@container>
2026-05-11 12:42:49 +0000

    a70f35dc-bab6-4b61-843d-22cc7ffbe3f8

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,7 @@
 // Omogoča namestitev na tablico (PWA), delno offline delovanje
 // in sinhronizacijo naročil, ko je povezava spet na voljo
 
-const CACHE_NAME = 'restaurantos-v2'
+const CACHE_NAME = 'restaurantos-v3'
 const STATIC_ASSETS = [
   '/',
   '/manifest.json',
